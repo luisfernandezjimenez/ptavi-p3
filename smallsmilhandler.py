dd998d028adbecc41c874b23c67e1d3e128f24b2
Luis  Fernandez Jimenez <lfernand@gamma16.aulas.gsyc.urjc.es>
2016-10-03 15:51:07 +0200

    lista de etiquetas, atributos y contenido de los atributos

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -24,67 +24,46 @@ class SmallSMILHandler(ContentHandler):
 
     def startElement(self, name, attrs):
         if name == 'root-layout':
-            self.width = attrs.get('width',"")
-            self.height = attrs.get('height',"")
-            self.backgroundcolor = attrs.get('background-color',"")
-            
-            self.atributos['width'] = self.width
-            self.atributos['height'] = self.height
-            self.atributos['background-color'] = self.backgroundcolor
+            self.atributos['width'] = attrs.get('width',"")
+            self.atributos['height'] = attrs.get('height',"")
+            self.atributos['background-color'] = attrs.get('background-color',"")
+            self.atributos['etiqueta'] = name
             self.lista.append(self.atributos)
             self.atributos = {}
         
         elif name == 'region':
-            self.id = attrs.get('id',"")
-            self.top = attrs.get('top',"")
-            self.bottom = attrs.get('bottom',"")
-            self.left = attrs.get('left',"")
-            self.right = attrs.get('right',"")
-            
-            self.atributos['id'] = self.id
-            self.atributos['top'] = self.top
-            self.atributos['bottom'] = self.bottom
-            self.atributos['left'] = self.left
-            self.atributos['right'] = self.right
+            self.atributos['id'] = attrs.get('id',"")
+            self.atributos['top'] = attrs.get('top',"")
+            self.atributos['bottom'] = attrs.get('bottom',"")
+            self.atributos['left'] = attrs.get('left',"")
+            self.atributos['right'] = attrs.get('right',"")
+            self.atributos['etiqueta'] = name
             self.lista.append(self.atributos)
             self.atributos = {}
             
         elif name == 'img':
-            self.src = attrs.get('src',"")
-            self.region = attrs.get('region',"")
-            self.begin = attrs.get('begin',"")
-            self.dur = attrs.get('dur',"")
-            
-            self.atributos['src'] = self.src
-            self.atributos['region'] = self.region
-            self.atributos['begin'] = self.begin
-            self.atributos['dur'] = self.dur
+            self.atributos['src'] = attrs.get('src',"")
+            self.atributos['region'] = attrs.get('region',"")
+            self.atributos['begin'] = attrs.get('begin',"")
+            self.atributos['dur'] = attrs.get('dur',"")
+            self.atributos['etiqueta'] = name
             self.lista.append(self.atributos)
             self.atributos = {}
             
         elif name == 'audio':
-            self.src = attrs.get('src',"")
-            self.begin = attrs.get('begin',"")
-            self.dur = attrs.get('dur',"")
-            
-            self.atributos['src'] = self.src
-            self.atributos['begin'] = self.begin
-            self.atributos['dur'] = self.dur
+            self.atributos['src'] = attrs.get('src',"")
+            self.atributos['begin'] = attrs.get('begin',"")
+            self.atributos['dur'] = attrs.get('dur',"")
+            self.atributos['etiqueta'] = name
             self.lista.append(self.atributos)
             self.atributos = {}
             
         elif name == 'textstream':
-            self.src = attrs.get('src',"")
-            self.region = attrs.get('region',"")
-            
-            self.atributos['src'] = self.src
-            self.atributos['region'] = self.region
+            self.atributos['src'] = attrs.get('src',"")
+            self.atributos['region'] = attrs.get('region',"")
+            self.atributos['etiqueta'] = name
             self.lista.append(self.atributos)
             self.atributos = {}
-        
-        #self.etiquetas.append(name)
-        
-        
 
     def get_tags(self):
         return self.lista

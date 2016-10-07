b2de17fa848a5d33643f039cf6e1b9d79591ce5c
Luis  Fernandez Jimenez <lfernand@alpha23.aulas.gsyc.urjc.es>
2016-10-07 13:34:01 +0200

    Acabado ejercicio 4

--- karaoke.py
+++ karaoke.py
@@ -10,26 +10,27 @@ import sys
 if __name__ == "__main__":
     try:
         ficherosmil = sys.argv[1]
+        parser = make_parser()
+        sHandler = SmallSMILHandler()
+        parser.setContentHandler(sHandler)
+        parser.parse(open(ficherosmil))
+        #obtengo las etiquetas del anterior ejercicio
+        mis_etiquetas = sHandler.get_tags()
+   
+        salida = "" 
+        for elemento in mis_etiquetas:
+            etiqueta = elemento[0]
+            atributos = elemento[1]
+            mis_atributos = ""
+            
+            #devuelvo la lista de claves del diccionario
+            for atributo in atributos:
+                mis_atributos = mis_atributos + '\t' + atributo + '="' + atributos[atributo] + '"'
+            
+            salida += etiqueta + mis_atributos + "\n"
+
+        print(salida)
     except IndexError:
         print("Usage: python3 karaoke.py file.smil")
         
-    parser = make_parser()
-    sHandler = SmallSMILHandler()
-    parser.setContentHandler(sHandler)
-    parser.parse(open(ficherosmil))
-    #obtengo las etiquetas del anterior ejercicio
-    mis_etiquetas = sHandler.get_tags()
-   
-    salida = "" 
-    for elemento in mis_etiquetas:
-        etiqueta = elemento[0]
-        atributos = elemento[1]
-        mis_atributos = ""
-            
-        #devuelvo la lista de claves del diccionario
-        for atributo in atributos:
-            mis_atributos = mis_atributos + '\t' + atributo + '="' + atributos[atributo] + '"'
-            
-        salida += etiqueta + mis_atributos + "\n"
 
-    print(salida)

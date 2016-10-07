281b0292ca1e9d04e869e80fb7fa9ebb566adf04
Luis  Fernandez Jimenez <lfernand@alpha23.aulas.gsyc.urjc.es>
2016-10-07 13:31:38 +0200

    Ejercicio 4

---
+++ karaoke.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+# -*- encoding: utf-8 -*-
+
+from xml.sax.handler import ContentHandler
+from xml.sax import make_parser
+from smallsmilhandler import SmallSMILHandler
+import sys
+
+
+if __name__ == "__main__":
+    try:
+        ficherosmil = sys.argv[1]
+    except IndexError:
+        print("Usage: python3 karaoke.py file.smil")
+        
+    parser = make_parser()
+    sHandler = SmallSMILHandler()
+    parser.setContentHandler(sHandler)
+    parser.parse(open(ficherosmil))
+    #obtengo las etiquetas del anterior ejercicio
+    mis_etiquetas = sHandler.get_tags()
+   
+    salida = "" 
+    for elemento in mis_etiquetas:
+        etiqueta = elemento[0]
+        atributos = elemento[1]
+        mis_atributos = ""
+            
+        #devuelvo la lista de claves del diccionario
+        for atributo in atributos:
+            mis_atributos = mis_atributos + '\t' + atributo + '="' + atributos[atributo] + '"'
+            
+        salida += etiqueta + mis_atributos + "\n"
+
+    print(salida)

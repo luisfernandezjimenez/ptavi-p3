7d3d8c90e3ed1cd8701c6f0fb56df1fe8824b9d2
Luis  Fernandez Jimenez <lfernand@alpha23.aulas.gsyc.urjc.es>
2016-10-07 13:07:33 +0200

    Acabado y con pep8

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -4,70 +4,31 @@
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
-class SmallSMILHandler(ContentHandler):
 
-    def __init__ (self):
-        self.width = ""
-        self.height = ""
-        self.backgroundcolor = ""
-        self.id = ""
-        self.top = ""
-        self.bottom = ""
-        self.left = ""
-        self.right = ""
-        self.src = ""
-        self.region = ""
-        self.begin = ""
-        self.dur = ""
+class SmallSMILHandler(ContentHandler):
+    def __init__(self):
+        self.root = ['width', 'height', 'background-color']
+        self.region = ['id', 'top', 'bottom', 'left', 'right']
+        self.img = ['src', 'region', 'begin', 'dur']
+        self.audio = ['src', 'begin', 'dur']
+        self.text = ['src', 'region']
+
+        self.etiquetas = {'root-layout': self.root, 'region': self.region,
+                          'img': self.img, 'audio': self.audio,
+                          'textstream': self.text}
         self.lista = []
-        self.atributos = {}
 
     def startElement(self, name, attrs):
-        if name == 'root-layout':
-            self.atributos['width'] = attrs.get('width',"")
-            self.atributos['height'] = attrs.get('height',"")
-            self.atributos['background-color'] = attrs.get('background-color',"")
-            self.atributos['etiqueta'] = name
-            self.lista.append(self.atributos)
-            self.atributos = {}
-        
-        elif name == 'region':
-            self.atributos['id'] = attrs.get('id',"")
-            self.atributos['top'] = attrs.get('top',"")
-            self.atributos['bottom'] = attrs.get('bottom',"")
-            self.atributos['left'] = attrs.get('left',"")
-            self.atributos['right'] = attrs.get('right',"")
-            self.atributos['etiqueta'] = name
-            self.lista.append(self.atributos)
-            self.atributos = {}
-            
-        elif name == 'img':
-            self.atributos['src'] = attrs.get('src',"")
-            self.atributos['region'] = attrs.get('region',"")
-            self.atributos['begin'] = attrs.get('begin',"")
-            self.atributos['dur'] = attrs.get('dur',"")
-            self.atributos['etiqueta'] = name
-            self.lista.append(self.atributos)
-            self.atributos = {}
-            
-        elif name == 'audio':
-            self.atributos['src'] = attrs.get('src',"")
-            self.atributos['begin'] = attrs.get('begin',"")
-            self.atributos['dur'] = attrs.get('dur',"")
-            self.atributos['etiqueta'] = name
-            self.lista.append(self.atributos)
-            self.atributos = {}
-            
-        elif name == 'textstream':
-            self.atributos['src'] = attrs.get('src',"")
-            self.atributos['region'] = attrs.get('region',"")
-            self.atributos['etiqueta'] = name
-            self.lista.append(self.atributos)
-            self.atributos = {}
+        if name in self.etiquetas:
+            self.dicc = {}
+            for atributo in self.etiquetas[name]:
+                self.dicc[atributo] = attrs.get(atributo, "")
+            self.lista.append(name)
+            self.lista.append(self.dicc)
 
     def get_tags(self):
         return self.lista
-            
+
 if __name__ == "__main__":
     parser = make_parser()
     Handler = SmallSMILHandler()

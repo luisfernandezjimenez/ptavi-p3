2e844c23edd5b4d10d2be3de1ce1a837371251e9
Luis  Fernandez Jimenez <lfernand@gamma16.aulas.gsyc.urjc.es>
2016-10-10 09:11:42 +0200

    Acabado ejercicio 6 y con pep8

--- karaoke.py
+++ karaoke.py
@@ -6,37 +6,55 @@ from xml.sax import make_parser
 from smallsmilhandler import SmallSMILHandler
 import sys
 import json
+import urllib
 
 
-if __name__ == "__main__":
-    try:
-        ficherosmil = sys.argv[1]
+class KaraokeLocal():
+    def __init__(self, fichero):
         parser = make_parser()
         sHandler = SmallSMILHandler()
         parser.setContentHandler(sHandler)
         parser.parse(open(ficherosmil))
         #obtengo las etiquetas del anterior ejercicio
-        mis_etiquetas = sHandler.get_tags()
-   
-        salida = "" 
-        for elemento in mis_etiquetas:
-            etiqueta = elemento[0]
-            atributos = elemento[1]
-            mis_atributos = ""
-            
-            #devuelvo la lista de claves del diccionario
-            for atributo in atributos:
-                if atributos[atributo] != "":
-                    mis_atributos = mis_atributos + '\t' + atributo + '="' + atributos[atributo] + '"'
-            
-            salida += etiqueta + mis_atributos + "\n"
+        self.mi_lista = sHandler.get_tags()
 
-        print(salida)
-        
-        ficherojson = ficherosmil.split('.')[0] + '.json'
-        json.dump(mis_etiquetas, open(ficherojson, 'w'))
+    def __str__(self):
+        salida = ''
+        for sublista in self.mi_lista:
+            etiqueta = sublista[0]
+            atributos = sublista[1]
+            for valor in atributos:
+                if atributos[valor] != '':  # los campos vacios no los quiero
+                    etiqueta += '\t' + valor + '="' + atributos[valor] + '"'
+            salida += etiqueta + '\n'
+        return salida
 
+    def to_json(self, fichero=''):
+        if not fichero:
+            fichero = 'local.json'
+        else:
+            fichero = fichero.split('.')[0] + '.json'
+
+        json.dump(self.mi_lista, open(fichero, 'w'))
+
+    def do_local(self):
+        for sublista in self.mi_lista:
+            atributos = sublista[1]
+            for clave in atributos:
+                if clave == 'src' and atributos[clave][:7] == 'http://':
+                #descargo el contenido de src y recorto el string por el final
+                    urllib.request.urlretrieve(atributos[clave],
+                                               atributos[clave].split('/')[-1])
+                    atributos[clave] = atributos[clave].split('/')[-1]
+
+if __name__ == "__main__":
+    try:
+        ficherosmil = sys.argv[1]
+        lista = KaraokeLocal(ficherosmil)
+        print(lista)
+        lista.to_json(ficherosmil)
+        lista.do_local()
+        lista.to_json()
+        print(lista)
     except IndexError:
         print("Usage: python3 karaoke.py file.smil")
-        
-
